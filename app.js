93c0e76449a9db7760b29155af99db38f0a44ad2
luis <luistenan@gmail.com>
2023-04-20 18:27:22 -0400

    Add basic app structure

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const { logger } = require('./utils/logger');
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev', { stream: logger.stream }));
+
+module.exports = app;
