d046b083ed74deaff4bc69e08d726b0310dcb1af
luis <luistenan@gmail.com>
2023-04-27 12:39:19 -0400

    Add file to populates some tests models
    Add feature to populate movies.
    Add featuer to populate reviews.

---
+++ populatedb.js
@@ -0,0 +1,205 @@
+require('dotenv').config();
+const Movie = require('./models/movie');
+const Review = require('./models/review');
+
+console.log(
+  'This script populates some test movies, users, reviews, rates, lists and watchlists to your database. Specified database URI below variable. To run just: node populatedb',
+);
+
+'e.g.: mongodb+srv://<user>:<password>@cluster.44q9kcx.mongodb.net/database_name?retryWrites=true&w=majority';
+const URI = process.env.DEV_MONGODB_URI;
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false); // Prepare for Mongoose 7
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  console.log('Debug: About to connect');
+  await mongoose.connect(URI);
+  console.log('Debug: Should be connected?');
+  await addInitialMovies();
+  await addInitialReviews();
+  console.log('Debug: Closing mongoose');
+  mongoose.connection.close();
+}
+
+// MOVIES 16 movies
+const initialMovies = [
+  {
+    name: 'The Fast and the Furious',
+    photo: 'https://image.tmdb.org/t/p/w185//lgCEntS9mHagxdL5hb3qaV49YTd.jpg',
+    description: "Dominic Toretto is a Los Angeles street racer suspected of masterminding a series of big-rig hijackings. When undercover cop Brian O'Conner infiltrates Toretto's iconoclastic crew, he falls for Toretto's sister and must choose a side: the gang or the LAPD.",
+    date: new Date(),
+    release_date: '2001-06-22',
+    idTMDB: 9799,
+    id: '6447e80aa1f0cd363649d594',
+  },
+  {
+    name: 'The Fast and the Furious: Tokyo Drift',
+    photo: 'https://image.tmdb.org/t/p/w185//cm2ffqb3XovzA5ZSzyN3jnn8qv0.jpg',
+    description: 'In order to avoid a jail sentence, Sean Boswell heads to Tokyo to live with his military father. In a low-rent section of the city, Shaun gets caught up in the underground world of drift racing',
+    date: new Date(),
+    release_date: '2006-06-03',
+    idTMDB: 9615,
+    id: '6447e80aa1f0cd363649d595',
+  },
+  {
+    name: 'The Fast and the Furious',
+    photo: 'https://image.tmdb.org/t/p/w185//731NMB4a85McIYcftPtbH4D3l5x.jpg',
+    description: "A man wrongly imprisoned for murder breaks out of jail. He wants to clear his name, but with the police pursuing him, he's forced to take a beautiful young woman, driving a fast sports car, hostage and slip into a cross-border sports car race to try to make it to Mexico before the police get him.",
+    date: new Date(),
+    release_date: '1954-11-01',
+    idTMDB: 20174,
+    id: '6447e80aa1f0cd363649d596',
+  },
+  {
+    name: 'Furious and Fast: The Story of Fast Music and the Patiphone',
+    photo: 'https://image.tmdb.org/t/p/w185//d4Rf3bD5T9fxVGQETRoCy6CJ5GM.jpg',
+    description: 'A music documentary about the independent "Fast Music" label and "The Patiphone" club in Tel Aviv from the late 90\'s till the late 00\'s.',
+    date: new Date(),
+    release_date: '2022-06-08',
+    idTMDB: 985445,
+  },
+  {
+    name: 'Tasmanian Devil: The Fast and Furious Life of Errol Flynn',
+    photo: 'https://image.tmdb.org/t/p/w185//zfbWl3dqLfMVWpn9nnQ0dUHgLbg.jpg',
+    description: "The story of Tasmanian-born actor Errol Flynn whose short & flamboyant life, full of scandals, adventures, loves and excess was largely played out in front of the camera - either making movies or filling the newsreels and gossip magazines. Tragically he was dead from the effects of drugs and alcohol by the time he was only 50 & the myths live on. But there is another side of Flynn that is less well known - his ambitions to be a serious writer and newspaper correspondent, his documentary films and his interest in the Spanish Civil War and Castro's Cuba",
+    date: new Date(),
+    release_date: '2007-10-17',
+    idTMDB: 467096,
+  },
+  {
+    name: 'F9',
+    photo: 'https://image.tmdb.org/t/p/w185//bOFaAXmWWXC3Rbv4u4uM9ZSzRXP.jpg',
+    description: "Dominic Toretto and his crew battle the most skilled assassin and high-performance driver they've ever encountered: his forsaken brother.",
+    date: new Date(),
+    release_date: '2021-05-19',
+    idTMDB: 385128,
+  },
+  {
+    name: 'The Fate of the Furious',
+    photo: 'https://image.tmdb.org/t/p/w185//dImWM7GJqryWJO9LHa3XQ8DD5NH.jpg',
+    description: 'When a mysterious woman seduces Dom into the world of crime and a betrayal of those closest to him, the crew face trials that will test them as never before.',
+    date: new Date(),
+    release_date: '2017-04-12',
+    idTMDB: 337339,
+  },
+  {
+    name: 'Fast & Furious Presents: Hobbs & Shaw',
+    photo: 'https://image.tmdb.org/t/p/w185//qRyy2UmjC5ur9bDi3kpNNRCc5nc.jpg',
+    description: "Ever since US Diplomatic Security Service Agent Hobbs and lawless outcast Shaw first faced off, they just have traded smack talk and body blows. But when cyber-genetically enhanced anarchist Brixton's ruthless actions threaten the future of humanity, they join forces to defeat him.",
+    date: new Date(),
+    release_date: '2019-08-01',
+    idTMDB: 384018,
+  },
+  {
+    name: 'Furious 7',
+    photo: 'https://image.tmdb.org/t/p/w185//wurKlC3VKUgcfsn0K51MJYEleS2.jpg',
+    description: 'Deckard Shaw seeks revenge against Dominic Toretto and his family for his comatose brother.',
+    date: new Date(),
+    release_date: '2015-04-01',
+    idTMDB: 168259,
+  },
+  {
+    name: 'Superfast!',
+    photo: 'https://image.tmdb.org/t/p/w185//iuIWl90qCpoxv6g775JB6Kg0m86.jpg',
+    description: "Undercover cop Lucas White joins Vin Serento's LA gang of illegal street racers. They are fast and they are furious and they plan to double cross LA crime kingpin Juan Carlos de la Sol who hides his cash in a downtown Taco Bell. The gang's outrageous plan is as daring as it is ridiculous and will see them towing the whole restaurant, at crazy speeds.",
+    date: new Date(),
+    release_date: '2015-03-05',
+    idTMDB: 325358,
+  },
+  {
+    name: 'Fast & Furious',
+    photo: 'https://image.tmdb.org/t/p/w185//AmY8rE2HzWzZs5S81CygXXYDjki.jpg',
+    description: "When a crime brings them back to L.A., fugitive ex-con Dom Toretto reignites his feud with agent Brian O'Conner. But as they are forced to confront a shared enemy, Dom and Brian must give in to an uncertain new trust if they hope to outmaneuver him. And the two men will find the best way to get revenge: push the limits of what's possible behind the wheel.",
+    date: new Date(),
+    release_date: '2009-04-02',
+    idTMDB: 13804,
+  },
+  {
+    name: 'Fast Five',
+    photo: 'https://image.tmdb.org/t/p/w185//gEfQjjQwY7fh5bI4GlG0RrBu7Pz.jpg',
+    description: "Former cop Brian O'Conner partners with ex-con Dom Toretto on the opposite side of the law. Since Brian and Mia Toretto broke Dom out of custody, they've blown across many borders to elude authorities. Now backed into a corner in Rio de Janeiro, they must pull one last job in order to gain their freedom.",
+    date: new Date(),
+    release_date: '2011-04-20',
+    idTMDB: 51497,
+  },
+  {
+    name: 'Fast & Furious 6',
+    photo: 'https://image.tmdb.org/t/p/w185//n31VRDodbaZxkrZmmzyYSFNVpW5.jpg',
+    description: 'Hobbs has Dominic and Brian reassemble their crew to take down a team of mercenaries: Dominic unexpectedly gets convoluted also facing his presumed deceased girlfriend, Letty.',
+    date: new Date(),
+    release_date: '2013-05-21',
+    idTMDB: 82992,
+  },
+  {
+    name: '2 Fast 2 Furious',
+    photo: 'https://image.tmdb.org/t/p/w185//6nDZExrDKIXvSAghsFKVFRVJuSf.jpg',
+    description: 'It\'s a major double-cross when former police officer Brian O\'Conner teams up with his ex-con buddy Roman Pearce to transport a shipment of "dirty" money for shady Miami-based import-export dealer Carter Verone. But the guys are actually working with undercover agent Monica Fuentes to bring Verone down.',
+    date: new Date(),
+    release_date: '2003-06-05',
+    idTMDB: 584,
+  },
+  {
+    name: 'The Turbo Charged Prelude for 2 Fast 2 Furious',
+    photo: 'https://image.tmdb.org/t/p/w185//bztZ5NWmsT7oq0vCWGQGWxd10Gf.jpg',
+    description: "Turbo-Charged Prelude is a 2003 short film, directed by Philip Atwell, featuring Paul Walker, reprising his role as Brian O'Conner, in a short series of sequences which bridge The Fast and The Furious with its first sequel, 2 Fast 2 Furious.",
+    date: new Date(),
+    release_date: '2003-06-03',
+    idTMDB: 77959,
+  },
+  {
+    name: 'Los Bandoleros',
+    photo: 'https://image.tmdb.org/t/p/w185//lpYaCwJWPvpQfzUy2vgKJ4e1IEN.jpg',
+    description: 'The film tells the back story about the characters and events leading up to the explosive oil truck heist in Fast &amp; Furious.',
+    date: new Date(),
+    release_date: '2009-07-28',
+    idTMDB: 253835,
+  },
+];
+
+async function addInitialMovies() {
+  await Movie.deleteMany({});
+
+  const movieObjects = initialMovies.map((movie) => new Movie(movie));
+  const promiseArray = movieObjects.map((movie) => movie.save());
+  await Promise.all(promiseArray);
+  console.log('Added movies');
+}
+
+// REVIEWS 3
+const initialReviews = [
+// First user 6418e218c4aeca730255a083 review of movie initialMovies[0]
+  {
+    title: 'First review made',
+    body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vita',
+    date: new Date(),
+    userId: '6418e218c4aeca730255a083',
+    movieId: '6447e80aa1f0cd363649d594',
+  },
+  // First user 6418e218c4aeca730255a083 review of movie initialMovies[1]
+  {
+    title: 'Second review made',
+    body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vita',
+    date: new Date(),
+    userId: '6418e218c4aeca730255a083',
+    movieId: '6447e80aa1f0cd363649d595',
+  },
+  // Second user 6418e218c4aeca730255a084 review of movie initialMovies[0]
+  {
+    title: 'Second review made',
+    body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitaLorem ipsum dolor sit amet, consectetuer adipiscing entum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vita',
+    date: new Date(),
+    userId: '6418e218c4aeca730255a084',
+    movieId: '6447e80aa1f0cd363649d594',
+  }];
+
+async function addInitialReviews() {
+  await Review.deleteMany({});
+
+  const reviewObjects = initialReviews.map((review) => new Review(review));
+  const promiseArray = reviewObjects.map((review) => review.save());
+  await Promise.all(promiseArray);
+  console.log('Added reviews');
+}
