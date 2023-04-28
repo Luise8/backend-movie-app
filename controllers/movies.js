8e63e73b6a56000b79a6d489dc9d8c35cd709e86
luis <luistenan@gmail.com>
2023-04-28 15:05:45 -0400

    Add pagination to routes of movies and reviews

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,9 +5,48 @@ const { logger } = require('../utils/logger');
 
 moviesRouter.get('/', async (request, response, next) => {
   try {
-    const movieList = await Movie.find({}).limit(10).sort({ rateAverage: -1, date: -1 }).exec();
+    // Check valid query params
+    if ((!/^\d+$/.test(request.query.pageSize) && request.query.pageSize !== undefined) || (!/^\d+$/.test(request.query.page) && request.query.page !== undefined)) {
+      logger.debug('🚀 ~ file: movies.js:12 ~ moviesRouter.get ~ request.query.pageSize: %O', request.query.pageSize);
+      response.status(400).end();
+      return;
+    }
+
+    // Get page and pageSize values
+    const pageSize = request.query.pageSize
+      && parseInt(request.query.pageSize, 10) <= 30 ? parseInt(request.query.pageSize, 10) : 10;
+    const page = request.query.page ? parseInt(request.query.page, 10) : 0;
+
+    // Get movie count and movies
+    const count = await Movie.find({}).count();
+    const movieList = await Movie.find({}).limit(pageSize)
+      .sort({ rateAverage: -1, date: -1 }).skip(pageSize * page)
+      .exec();
+
+    // Get number of prev page
+    let prevPage;
+    if (page === 0) {
+      prevPage = '';
+    } else if ((pageSize * (page - 1)) > count) {
+      if (Number.isInteger(count / pageSize)) {
+        prevPage = (count / pageSize) - 1;
+      } else {
+        prevPage = parseInt(count / pageSize, 10);
+      }
+    } else {
+      prevPage = page - 1;
+    }
+
+    const resultMovies = {
+      total: count,
+      page_size: pageSize,
+      page,
+      prev_page: page === 0 ? prevPage : `/movies?page=${prevPage}&page_size=${pageSize}`,
+      next_page: (pageSize * (page + 1)) < count ? `/movies?page=${page + 1}&page_size=${pageSize}` : '',
+      results: movieList,
+    };
     logger.debug('%O', movieList);
-    response.json(movieList);
+    response.json(resultMovies);
   } catch (exception) {
     next(exception);
   }
@@ -28,29 +67,82 @@ moviesRouter.get('/:id', async (request, response, next) => {
 
 moviesRouter.get('/:id/reviews', async (request, response, next) => {
   try {
+    // Check valid query params
+    if ((!/^\d+$/.test(request.query.pageSize) && request.query.pageSize !== undefined) || (!/^\d+$/.test(request.query.page) && request.query.page !== undefined)) {
+      logger.debug('🚀 ~ file: movies.js:12 ~ moviesRouter.get ~ request.query.pageSize: %O', request.query.pageSize);
+      response.status(400).end();
+      return;
+    }
+
+    // Get page and pageSize values
+    const pageSize = request.query.pageSize
+      && parseInt(request.query.pageSize, 10) <= 30 ? parseInt(request.query.pageSize, 10) : 10;
+    const page = request.query.page ? parseInt(request.query.page, 10) : 0;
+
+    // Get movie
     const movie = await Movie.findOne({ idTMDB: request.params.id }).lean();
+
+    // If exist
     if (movie) {
-      const reviews = await Review.find({ movieId: movie._id });
+      // Get count of reviews of movie and get reviews
+      const count = await Review.find({ movieId: movie._id }).count();
+      const reviews = await Review.find({ movieId: movie._id }).limit(pageSize)
+        .sort({ date: -1 }).skip(pageSize * page)
+        .exec();
       logger.debug('🚀 ~ file: movies.js:34 ~ moviesRouter.get ~ reviews:%O', reviews);
+
+      // Get number of prev page
+      let prevPage;
+      if (page === 0) {
+        prevPage = '';
+      } else if ((pageSize * (page - 1)) > count) {
+        if (Number.isInteger(count / pageSize)) {
+          prevPage = (count / pageSize) - 1;
+        } else {
+          prevPage = parseInt(count / pageSize, 10);
+        }
+      } else {
+        prevPage = page - 1;
+      }
+
+      // If there are not reviews with the current page and pageSize
       if (reviews.length === 0) {
         response.json({
-          idTMDB: movie.idTMDB,
-          name: movie.name,
-          release_date: movie.release_date,
-          photo: movie.photo,
-          rateAverage: movie.rateAverage,
-          reviews: [],
+          movie_details: {
+            idTMDB: movie.idTMDB,
+            name: movie.name,
+            release_date: movie.release_date,
+            photo: movie.photo,
+            rateAverage: movie.rateAverage,
+          },
+          total: count,
+          page_size: pageSize,
+          page,
+          prev_page: page === 0 ? prevPage : `/movies/${request.params.id}/reviews?page=${prevPage}&page_size=${pageSize}`,
+          next_page: (pageSize * (page + 1)) < count ? `/movies/${request.params.id}/reviews?page=${page + 1}&page_size=${pageSize}` : '',
+          results: [],
         });
+
+        // If there are reviews with the current page and pageSize
       } else {
         response.json({
-          idTMDB: movie.idTMDB,
-          name: movie.name,
-          release_date: movie.release_date,
-          photo: movie.photo,
-          rateAverage: movie.rateAverage,
-          reviews,
+          movie_details: {
+            idTMDB: movie.idTMDB,
+            name: movie.name,
+            release_date: movie.release_date,
+            photo: movie.photo,
+            rateAverage: movie.rateAverage,
+          },
+          total: count,
+          page_size: pageSize,
+          page,
+          prev_page: page === 0 ? prevPage : `/movies/${request.params.id}/reviews?page=${prevPage}&page_size=${pageSize}`,
+          next_page: (pageSize * (page + 1)) < count ? `/movies/${request.params.id}/reviews?page=${page + 1}&page_size=${pageSize}` : '',
+          results: reviews,
         });
       }
+
+      // If movie does not exist
     } else {
       response.status(404).end();
     }
