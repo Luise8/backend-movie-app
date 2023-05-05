a837b2dc481685b62050eaf2dd3df36f18a8e454
luis <luistenan@gmail.com>
2023-05-05 17:31:07 -0400

    Add middleware to handle require logged routes.
    This will help later

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,6 +18,13 @@ const errorHandler = (error, request, response, next) => {
   return null;
 };
 
+const isAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(401).json({ msg: 'You are not authorized to view this resource' });
+  }
+};
 module.exports = {
   unknownEndpoint,
   errorHandler,
