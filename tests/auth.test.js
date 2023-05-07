58535f5c37f342bbe327fa6d9325b438f29cc52b
luis <luistenan@gmail.com>
2023-05-07 17:25:21 -0400

    Add test of auth controller

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -20,53 +20,107 @@ beforeAll(async () => {
     store: sessionStore,
   }));
 
-  app.use('/api/v1.0/auth', authRouter);
   app.use(passport.initialize());
   app.use(passport.session());
+  app.use('/api/v1.0/auth', authRouter);
 });
-const api = request(app);
+const api = request.agent(app);
+
+beforeEach(async () => {
+  // To remove the session after every test
+  await api
+    .post('/api/v1.0/auth/logout').send();
+});
 
 describe('when there is initially some users saved in db', () => {
   beforeAll(async () => {
     await addInitialUsers(); // Add 13 users
   });
 
-  it('succeeds with valid data', async () => {
-    const res = await api
-      .post('/api/v1.0/auth/login')
-      .send({
-        username: initialUsers[0].username,
-        password: initialUsers[0].username, // The password is the same that username
-      })
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+  describe('login', () => {
+    it('succeeds with valid data', async () => {
+      const res = await api
+        .post('/api/v1.0/auth/login')
+        .send({
+          username: initialUsers[0].username,
+          password: initialUsers[0].username, // The password is the same that username
+        })
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    // The info of the currentSession
-    expect(res.body).toEqual({ currentSession: { isAuth: true, userId: initialUsers[0]._id } });
+      // The info of the currentSession
+      expect(res.body).toEqual({ currentSession: { isAuth: true, userId: initialUsers[0]._id } });
+    });
+
+    it('fails with status 400 and errors prop with errors if data is invalid', async () => {
+      const res = await api
+        .post('/api/v1.0/auth/login')
+        .send({
+          username: initialUsers[0].username,
+          password: 'Invalid password with withespaces', // The password is the same that username
+        })
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
+
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.length).toBeGreaterThan(0);
+    });
+
+    it('fails with status 401 if username or password are wrong', async () => {
+      await api
+        .post('/api/v1.0/auth/login')
+        .send({
+          username: initialUsers[0].username,
+          password: 'wrongPassword', // The password must be the same that username
+        })
+        .expect(401);
+    });
   });
 
-  it('fails with status 400 and errors prop with errors if data is invalid', async () => {
-    const res = await api
-      .post('/api/v1.0/auth/login')
-      .send({
-        username: initialUsers[0].username,
-        password: 'Invalid password with withespaces', // The password is the same that username
-      })
-      .expect(400)
-      .expect('Content-Type', /application\/json/);
+  describe('logout', () => {
+    it('successful with return of current session user null and isAuth false', async () => {
+      // login user
+      await api
+        .post('/api/v1.0/auth/login')
+        .send({
+          username: initialUsers[0].username,
+          password: initialUsers[0].username, // The password is the same that username
+        }).expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    expect(res.body.errors).toBeDefined();
-    expect(res.body.errors.length).toBeGreaterThan(0);
+      const res = await api
+        .post('/api/v1.0/auth/logout')
+        .send();
+
+      // The info of the currentSession
+      expect(res.body).toEqual({ currentSession: { isAuth: false, userId: null } });
+    });
   });
 
-  it('fails with status 401 if username or password are wrong', async () => {
-    await api
-      .post('/api/v1.0/auth/login')
-      .send({
-        username: initialUsers[0].username,
-        password: 'wrongPassword', // The password must be the same that username
-      })
-      .expect(401);
+  describe('status', () => {
+    it('return logged in session when user is logged in', async () => {
+      // login user
+      await api
+        .post('/api/v1.0/auth/login')
+        .send({
+          username: initialUsers[0].username,
+          password: initialUsers[0].username, // The password is the same that username
+        })
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+
+      const res = await api
+        .get('/api/v1.0/auth/status');
+
+      expect(res.body).toEqual({ currentSession: { isAuth: true, userId: initialUsers[0]._id } });
+    });
+
+    it('return session not logged in when the user is not logged in', async () => {
+      const res = await api
+        .get('/api/v1.0/auth/status');
+
+      expect(res.body).toEqual({ currentSession: { isAuth: false, userId: null } });
+    });
   });
 });
 
